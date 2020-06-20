bf1b3a75e15d1b7bf57d00bb6d0588120afbe8c7
maigohara <maigohara-1103@outlook.jp>
2020-06-19 18:58:07 -0700

    add formik

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const SignIn = ({ auth, authError, signIn }) => {
   const [state, setState] = useState({
@@ -20,25 +22,56 @@ const SignIn = ({ auth, authError, signIn }) => {
     signIn(state);
   };
 
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Enter a correct email')
+      .required('Enter an email'),
+    password: Yup.string()
+      .required('Enter a password'),
+  })
+
   if (auth.uid) return <Redirect to="/" />;
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <p>Email</p>
-          <input type="email" id="email" onChange={handleChange} />
-        </div>
-        <div>
-          <p>Password</p>
-          <input type="password" id="password" onChange={handleChange} />
-        </div>
-        <div>
-          <button>Sign In</button>
-          <div>{authError ? <p>{authError}</p> : null}</div>
-        </div>
-      </form>
-    </div>
+    <>
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+      >
+        <>
+          <Form>
+            <p>Email</p>
+            <Field
+              name="email"
+              type="email"
+              id="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <p>Password</p>
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              placeholder="password"
+              onChange={handleChange}
+            />
+            {/* <div>
+              <input type="email" id="email" onChange={handleChange} />
+            </div> */}
+            {/* <div>
+              <p>Password</p>
+              <input type="password" id="password" onChange={handleChange} />
+            </div> */}
+          </Form>
+          <div>
+            <button>Sign In</button>
+            <div>{authError ? <p>{authError}</p> : null}</div>
+          </div>
+        </>
+      </Formik>
+    </>
   );
 };
 
